58b7ee41a1582b8c0a94b1dc16a19e442ba2560d
syntax-lab <syntax.its.me@gmail.com>
2020-03-07 15:53:07 +0100

    bug fix

--- CloseAllMenuStackedWindowsExceptActiveOne/CloseAllMenuStackedWindowsExceptActiveOne.ps1
+++ CloseAllMenuStackedWindowsExceptActiveOne/CloseAllMenuStackedWindowsExceptActiveOne.ps1
@@ -20,34 +20,54 @@ Add-Type @"
     }
 "@
 
-Set-Variable MOD_WIN      -option Constant -value 0x0008
-Set-Variable MOD_SHIFT    -option Constant -value 0x0004
-Set-Variable MOD_NOREPEAT -option Constant -value 0x4000
+Set-Variable MOD_WIN      -Option Constant -Value 0x0008
+Set-Variable MOD_SHIFT    -Option Constant -Value 0x0004
+Set-Variable MOD_NOREPEAT -Option Constant -Value 0x4000
 
-Set-Variable VK_ESC       -option Constant -value 0x001B
-Set-Variable VK_OEM_MINUS -option Constant -value 0x00BD
+Set-Variable VK_ESC       -Option Constant -Value 0x001B
+Set-Variable VK_OEM_MINUS -Option Constant -Value 0x00BD
 
-Set-Variable SW_MINIMIZE  -option Constant -value 0x0006
-Set-Variable SW_RESTORE   -option Constant -value 0x0009
+Set-Variable SW_MINIMIZE  -Option Constant -Value 0x0006
+Set-Variable SW_RESTORE   -Option Constant -Value 0x0009
 
-Set-Variable WM_HOTKEY    -option Constant -value 0x0312
+Set-Variable WM_HOTKEY    -Option Constant -Value 0x0312
 
-Set-Variable HK_CLOSE_STACKED_WND    -option Constant -value 0x0000
-Set-Variable HK_MINIMIZE_STACKED_WND -option Constant -value 0x0001
-Set-Variable HK_RESTORE_STACKED_WND  -option Constant -value 0x0002
+Set-Variable HK_CLOSE_STACKED_WND    -Option Constant -Value 0x0000
+Set-Variable HK_MINIMIZE_STACKED_WND -Option Constant -Value 0x0001
+Set-Variable HK_RESTORE_STACKED_WND  -Option Constant -Value 0x0002
 
-if([WinApiDlls]::RegisterHotKey(0, $HK_CLOSE_STACKED_WND, $MOD_WIN -Bor $MOD_NOREPEAT, $VK_ESC) -And
+if([WinApiDlls]::RegisterHotKey(0, $HK_CLOSE_STACKED_WND, $MOD_WIN    -Bor $MOD_NOREPEAT, $VK_ESC)       -And
    [WinApiDlls]::RegisterHotKey(0, $HK_MINIMIZE_STACKED_WND, $MOD_WIN -Bor $MOD_NOREPEAT, $VK_OEM_MINUS) -And
-   [WinApiDlls]::RegisterHotKey(0, $HK_RESTORE_STACKED_WND, $MOD_WIN -Bor $MOD_SHIFT -Bor $MOD_NOREPEAT, $VK_OEM_MINUS)){
-    Write-Host("Use 'WIN+ESC' to close all stacked windows except active one.")
-    Write-Host("Use 'WIN+-' to minimize all stacked windows except active one.")
-    Write-Host("Use 'WIN+SHIFT+-' to restore all minimized stacked windows except active one.")
+   [WinApiDlls]::RegisterHotKey(0, $HK_RESTORE_STACKED_WND, $MOD_WIN  -Bor $MOD_SHIFT -Bor $MOD_NOREPEAT, $VK_OEM_MINUS)){
+    Write-Host("Press 'WIN+ESC' to close all stacked windows except active one.")
+    Write-Host("Press 'WIN+-' to minimize all stacked windows except active one.")
+    Write-Host("Press 'WIN+SHIFT+-' to restore all minimized stacked windows except active one.")
 }else{
-    Write-Host("Failed to register Hot Keys, application terminated.")
+    Write-Host("Failed to register hot keys, script terminated.")
     exit(1)
 }
 
-$msg = [WinApiMsgStruct]@{ hWnd = [System.IntPtr]0; message = 0; wParam = [System.IntPtr]0; lParam = [System.IntPtr]0; time = 0; }
+function Find-AllChilds{
+    param(
+        [parameter(Mandatory = $true, ValueFromPipeline = $false)]
+        [int]$child_process_id
+    )
+
+    if($child_process_id -ne $null){
+        Get-CimInstance -Class Win32_Process -Filter "ParentProcessId=$child_process_id" | ForEach-Object {
+            Find-AllChilds($_.ProcessId)
+        }
+        #NOTE: sometimes child processes enforce parent to terminate earlier
+        Stop-Process -Id $child_process_id -ErrorAction SilentlyContinue
+    }
+}
+
+function Get-RootActiveWindows{
+    $explorer_process_id = (Get-Process "explorer").Id
+    return Get-CimInstance -Class Win32_Process -Filter "ParentProcessId=$explorer_process_id" | ? {
+        $_.ProcessId -ne $active_window_id -And $_.ProcessName -eq $active_window_module_name
+    }
+}
 
 function Set-WindowsState{
     param(
@@ -60,39 +80,33 @@ function Set-WindowsState{
         $active_window_info = Get-Process | ? { $_.MainWindowHandle -eq $active_window_hwnd }
         if($active_window_info){
             $active_window_id = $active_window_info.Id
-            $active_window_name = $active_window_info.ProcessName
-        }
-    }
-
-    switch($wParam){
-        $HK_CLOSE_STACKED_WND{
-            $processes_by_name = Get-Process $active_window_name | ? { $_.Id -ne $active_window_id }
-            if($processes_by_name){
-                $processes_by_name | ? {
-                    $parent_id = $_.Id; Get-CimInstance -Class Win32_Process -Filter "ParentProcessId=$parent_id" | ? {
-                        Stop-Process $_.ProcessId
+            $active_window_module_name = $active_window_info.MainModule.ModuleName
+            $active_window_name = $active_window_info.Name
+            switch($wParam){
+                $HK_CLOSE_STACKED_WND{
+                    Get-RootActiveWindows | ? {
+                        Find-AllChilds($_.ProcessId)
                     }
-                    Stop-Process $parent_id
+                }
+                $HK_MINIMIZE_STACKED_WND{
+                    Get-RootActiveWindows | ? {
+                        $main_window_handle = (Get-Process -Id $_.ProcessId).MainWindowHandle
+                        [WinApiDlls]::ShowWindow($main_window_handle, $SW_MINIMIZE)
+                    } | Out-Null
+                }
+                $HK_RESTORE_STACKED_WND{
+                    Get-RootActiveWindows | ? {
+                        $main_window_handle = (Get-Process -Id $_.ProcessId).MainWindowHandle
+                        [WinApiDlls]::ShowWindow($main_window_handle, $SW_RESTORE)
+                    } | Out-Null
+                    [void][WinApiDlls]::SetForegroundWindow($active_window_hwnd)
                 }
             }
         }
-        $HK_MINIMIZE_STACKED_WND{
-            Get-Process $active_window_name | Select-Object mainwindowhandle | ? {
-                if($_.MainWindowHandle -ne $active_window_hwnd){
-                    [WinApiDlls]::ShowWindow($_.MainWindowHandle, $SW_MINIMIZE)
-                }
-            } | Out-Null
-        }
-        $HK_RESTORE_STACKED_WND{
-            Get-Process $active_window_name | Select-Object mainwindowhandle | ? {
-                if($_.MainWindowHandle -ne $active_window_hwnd){
-                    [WinApiDlls]::ShowWindow($_.MainWindowHandle, $SW_RESTORE)
-                }
-            } | Out-Null
-            [WinApiDlls]::SetForegroundWindow($active_window_hwnd) | Out-Null
-        }
     }
 }
+
+$msg = [WinApiMsgStruct]@{ hWnd = [System.IntPtr]0; message = 0; wParam = [System.IntPtr]0; lParam = [System.IntPtr]0; time = 0; }
 
 while([WinApiDlls]::GetMessage([ref]$msg, [System.IntPtr]0, 0, 0) -ne 0){
     if($msg.message -eq $WM_HOTKEY){
